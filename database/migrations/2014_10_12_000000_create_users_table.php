782c9bc1f5db3926b39ffad1f100df1215e6c7f4
andikaandra <andikaandra03@gmail.com>
2018-12-13 19:06:08 +0700

    new migrate status lomba user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('universitas')->nullable();
             $table->integer('cabang')->nullable();//1 imsso,2 imarc,3 inamsc, 4 hfgm
             $table->integer('cabang_spesifik')->nullable(); //1 simposium, 2 vid edukasi & pp, 3 litrev & rpp
-            $table->tinyInteger('verified')->default(0);
+            $table->tinyInteger('verified')->default(0); //verifikasi akun
+            $table->tinyInteger('lomba_verified')->default(0); //verifikasi oleh admin tentang lomba yang diikuti
             $table->string('email_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
