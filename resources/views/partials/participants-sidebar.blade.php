2754bb9e95a99797f22a5ec6e1407ceff9889335
adis azhar <adisazhar123@gmail.com>
2018-12-20 15:34:34 +0700

    sidebar bug

--- resources/views/partials/participants-sidebar.blade.php
+++ resources/views/partials/participants-sidebar.blade.php
@@ -23,39 +23,23 @@
         <i class="mdi mdi-home menu-icon"></i>
       </a>
     </li>
-    @if(Auth::user()->verified != 1)
-      <li class="nav-item unclickable" id="user-team">
-        <a class="nav-link" href="#">
+    @if(Auth::user()->lomba_verified)
+      <li class="nav-item" id="user-team">
+        <a class="nav-link" href="{{url('users/participants')}}">
           <span class="menu-title">
             My Team
           </span>
           <i class="mdi mdi-account-multiple menu-icon"></i>
         </a>
       </li>
-      <li class="nav-item unclickable" id="user-announcement">
+      <li class="nav-item" id="user-announcement">
         <a class="nav-link" href="#">
           <span class="menu-title">Announcements</span>
           <i class="mdi mdi-bell menu-icon"></i>
         </a>
       </li>
-
-      @else
-        <li class="nav-item" id="user-team">
-          <a class="nav-link" href="{{url('users/participants')}}">
-            <span class="menu-title">
-              My Team
-            </span>
-            <i class="mdi mdi-account-multiple menu-icon"></i>
-          </a>
-        </li>
-        <li class="nav-item" id="user-announcement">
-          <a class="nav-link" href="#">
-            <span class="menu-title">Announcements</span>
-            <i class="mdi mdi-bell menu-icon"></i>
-          </a>
-        </li>
-
     @endif
+    
     @if(Auth::user()->cabang==3 &&(Auth::user()->cabang_spesifik==2 || Auth::user()->cabang_spesifik==3))
       @if (Auth::user()->lomba_verified != 1)
         <li class="nav-item unclickable" id="user-files">
