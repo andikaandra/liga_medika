8705166eb31928431d3e3183575a4c3d8fde06ef
adis azhar <adisazhar123@gmail.com>
2018-12-23 20:04:54 +0700

    verif redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            if (Auth::user()->role == 1) {
+              return redirect('users');
+            } else {
+              return redirect('admin');
+            }
         }
 
         return $next($request);
