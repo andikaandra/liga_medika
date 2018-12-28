fa61e18a21d002da48a6e60371aedd59eb1da876
Adis <adisazhar123@gmail.com>
2018-12-29 02:43:39 +0700

    server updates

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_USERNAME', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
@@ -84,9 +84,11 @@ return [
     |
     */
 
+
     'username' => env('MAIL_USERNAME'),
 
     'password' => env('MAIL_PASSWORD'),
+
 
     /*
     |--------------------------------------------------------------------------
