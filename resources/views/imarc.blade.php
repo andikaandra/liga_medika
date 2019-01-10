eb4bf1aa79348f3ac70dff53ac2b4e7062e0205f
adis azhar <adisazhar123@gmail.com>
2019-01-10 19:08:26 +0700

    description

--- resources/views/imarc.blade.php
+++ resources/views/imarc.blade.php
@@ -72,7 +72,7 @@
 					<div class="col-lg-7 quote-right">
 						<h1 class="text-info mb-3">Band</h1>
 						<p class="justify text-justify">
-							Cabang Band akan diadakan pada hari Minggu, sebelum HFGM Closing Ceremony. Peserta akan diminta untuk membawakan dua lagu, lagu wajib yang sudah ditentukan panitia dan lagu bebas yang merupakan pilihan dari para peserta. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang beserta fasilitas distribusi musik digital serta tampil di acara closing Liga Medika (memperebutkan juara I, II, III)
+							Cabang Band akan diadakan pada hari Minggu, 28 April 2019, sebelum HFGM Closing Ceremony. Peserta akan diminta untuk membawakan dua lagu, lagu wajib yang sudah ditentukan panitia dan lagu bebas yang merupakan pilihan dari para peserta. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang beserta fasilitas distribusi musik digital serta tampil di acara closing Liga Medika (memperebutkan juara I, II, III)
 						</p>
 					</div>
 				</div>
@@ -82,7 +82,7 @@
 					<div class="col-lg-7 quote-left">
 							<h1 class="text-info mb-3">Fotografi</h1>
 						<p class="justify text-justify">
-							Cabang Fotografi akan diadakan bersamaan dengan HFGM Closing Ceremony. Hasil foto dari peserta akan disajikan dalam bentuk pameran foto dan dipamerkan bersamaan dengan acara HFGM dan penutupan dari HFGM. Dalam lomba foto akan ada tambahan mata acara lain berupa photo rally untuk para peserta HFGM. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang (memperebutkan juara I, II, dan III)
+								Submisi untuk Cabang Fotografi akan dibuka sampai sebelum rangkaian acara utama Liga Medika 2019 dimulai. Hasil foto dari peserta akan disajikan dalam bentuk pameran foto dan dipamerkan bersamaan dengan acara HFGM dan penutupan dari HFGM. Dalam lomba foto akan ada tambahan mata acara lain berupa photo rally untuk para peserta HFGM. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang (memperebutkan juara I, II, dan III).
 						</p>
 					</div>
 					<div class="col-lg-5 quote-right">
@@ -102,7 +102,7 @@
 					<div class="col-lg-7 quote-right">
 						<h1 class="text-info mb-3" style="font-weight: bold; font-size: 36px">Vocal Group</h1>
 						<p class="justify text-justify">
-							Cabang Vocal Group akan diadakan pada hari Sabtu. Acara pada cabang ini adalah kompetisi vocal group. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang (memperebutkan juara I, II, III)
+							Cabang Vocal Group akan diadakan pada hari Minggu, 29 April 2019. Peserta akan diminta untuk membawakan dua lagu, lagu wajib yang sudah ditentukan panitia dan lagu bebas yang sesuai dengan tema yang telah ditentukan. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang (memperebutkan juara I, II, III).
 						</p>
 					</div>
 				</div>
@@ -111,7 +111,7 @@
 					<div class="col-lg-7 quote-left">
 						<h1 class="text-info mb-3" style="font-weight: bold; font-size: 36px">Tari Tradisional</h1>
 						<p class="justify text-justify">
-							Cabang Tari Tradisional akan diadakan pada hari Sabtu. Terdapat dua mata acara pada cabang ini, yaitu lomba tari tradisional dan workshop tari tradisional bagi para peserta lomba. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang (memperebutkan juara I, II, III)
+							Cabang Tari Tradisional akan diadakan pada hari Sabtu, 27 April 2019. Terdapat dua mata acara pada cabang ini, yaitu lomba tari tradisional dan workshop tari tradisional bagi para peserta lomba. Pemenang dari cabang ini akan mendapatkan hadiah berupa uang (memperebutkan juara I, II, III)
 						</p>
 					</div>
 					<div class="col-lg-5 quote-right">
