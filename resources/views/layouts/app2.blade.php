2e2a67e748af5e670bf397b151e6888d844fdec7
andikaandra <andikaandra03@gmail.com>
2019-01-10 23:12:53 +0700

    typo

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -114,7 +114,7 @@
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
-                        </form> --}}
+                        </form>
                     @endguest
 
                     </ul>
