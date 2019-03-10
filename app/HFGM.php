5fd0f31693e6eec3ab88d0194df060aa842cf3d0
adis azhar <adisazhar123@gmail.com>
2019-03-10 23:11:52 +0700

    hfgm model

--- app/HFGM.php
+++ app/HFGM.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class HFGM extends Model
 {
     protected $table = "hfgm";
-    protected $fillable = ['user_id', 'type', 'nama', 'ktp', 'status_pembayaran', 'jumlah_tiket', 'gelombang'];
+    protected $fillable = ['user_id', 'type', 'nama', 'ktp', 'status_pembayaran', 'jumlah_tiket', 'gelombang', 'status_verif'];
 
     public function user() {
       return $this->belongsTo('App\User');
