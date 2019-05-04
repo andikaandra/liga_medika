ceec2a267d2bd7bc2b5247cdea8e3edb1ac40b41
andikaandra <andikaandra03@gmail.com>
2019-05-04 14:09:27 +0700

    test

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,7 +20,7 @@
         <nav aria-label="breadcrumb">
           <ul class="breadcrumb">
             <li class="breadcrumb-item active" aria-current="page">
-              <span></span>Overview
+              <span></span>
               <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
             </li>
           </ul>
